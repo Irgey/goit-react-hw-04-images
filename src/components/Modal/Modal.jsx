cb966a2e604f664e86bc1daec8a8fea83256ed6f
Irgey <116591589+Irgey@users.noreply.github.com>
2023-04-19 23:57:45 +0200

    refactor part 1

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,37 +1,45 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { createPortal } from 'react-dom';
 import styled from 'styled-components';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.onEscapeClick);
-  }
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.onEscapeClick);
-  }
+export const Modal = ({ onClose, children }) => {
+  // componentDidMount() {
+  //   window.addEventListener('keydown', this.onEscapeClick);
+  // }
+  useEffect(() => window.addEventListener('keydown', onEscapeClick));
 
-  onEscapeClick = ev => {
+  // componentWillUnmount() {
+  //   window.removeEventListener('keydown', this.onEscapeClick);
+  // }
+  useEffect(() => {
+    return () => {
+      window.removeEventListener('keydown', onEscapeClick);
+    };
+  });
+
+  /**
+   * Own methods
+   */
+  const onEscapeClick = ev => {
     if (ev.code === 'Escape') {
-      this.props.onClose();
+      onClose();
     }
   };
-  onOverlayClick = ev => {
+  const onOverlayClick = ev => {
     if (ev.target === ev.currentTarget) {
-      this.props.onClose();
+      onClose();
     }
   };
-  render() {
-    return createPortal(
-      <StyledOverlay onClick={this.onOverlayClick} id="backdrop">
-        <StyledModal>{this.props.children}</StyledModal>
-      </StyledOverlay>,
-      modalRoot
-    );
-  }
-}
+  return createPortal(
+    <StyledOverlay onClick={onOverlayClick} id="backdrop">
+      <StyledModal>{children}</StyledModal>
+    </StyledOverlay>,
+    modalRoot
+  );
+};
 
 Modal.propTypes = {
   children: PropTypes.oneOfType([
@@ -55,3 +63,63 @@ const StyledModal = styled.div`
   max-width: calc(100vw - 48px);
   max-height: calc(100vh - 24px);
 `;
+/**
+ * Class method
+ */
+// import { Component } from 'react';
+// import PropTypes from 'prop-types';
+// import { createPortal } from 'react-dom';
+// import styled from 'styled-components';
+
+// const modalRoot = document.querySelector('#modal-root');
+
+// export class Modal extends Component {
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.onEscapeClick);
+//   }
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.onEscapeClick);
+//   }
+
+//   onEscapeClick = ev => {
+//     if (ev.code === 'Escape') {
+//       this.props.onClose();
+//     }
+//   };
+//   onOverlayClick = ev => {
+//     if (ev.target === ev.currentTarget) {
+//       this.props.onClose();
+//     }
+//   };
+//   render() {
+//     return createPortal(
+//       <StyledOverlay onClick={this.onOverlayClick} id="backdrop">
+//         <StyledModal>{this.props.children}</StyledModal>
+//       </StyledOverlay>,
+//       modalRoot
+//     );
+//   }
+// }
+
+// Modal.propTypes = {
+//   children: PropTypes.oneOfType([
+//     PropTypes.arrayOf(PropTypes.element),
+//     PropTypes.element,
+//   ]),
+// };
+// const StyledOverlay = styled.div`
+//   position: fixed;
+//   top: 0;
+//   left: 0;
+//   width: 100vw;
+//   height: 100vh;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   background-color: rgba(0, 0, 0, 0.8);
+//   z-index: 1200;
+// `;
+// const StyledModal = styled.div`
+//   max-width: calc(100vw - 48px);
+//   max-height: calc(100vh - 24px);
+// `;
